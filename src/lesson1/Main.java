0a6f4b7693278b6baacf055c77f25161817962a4
Daronux66 <55246564+Daronux66@users.noreply.github.com>
2023-02-25 14:59:00 +0100

    Main class created

---
+++ src/lesson1/Main.java
@@ -0,0 +1,9 @@
+package lesson1;
+
+public class Main {
+	public static void main(String[] args) {
+		Student st= new Student("David","Rossi",
+				"Universidad de León","Computer Science");
+		System.out.println(st.toString());
+	}
+}
