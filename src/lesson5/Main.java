3634cea50b6de3262bc659c8136066dae509bbae
Daronux66 <55246564+Daronux66@users.noreply.github.com>
2023-04-03 01:47:36 +0200

    allowed input in garages´s dimensional parameters

--- src/lesson5/Main.java
+++ src/lesson5/Main.java
@@ -1,17 +1,25 @@
 package lesson5;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		Garage g = new Garage(5, 4);
-		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the rows of the garage:");
+		int rows = sc.nextInt();
+		System.out.println("Enter the cols of the garage:");
+		int cols = sc.nextInt();
+		Garage g = new Garage(rows, cols);
+
 		System.out.println("*** GENERATED GARAGE ***");
 		System.out.println("DISPLAY MILEAGES:");
 		System.out.println(g.displayMileAges());
 		System.out.println("DISPLAY PLATES:");
 		System.out.println(g.displayPlates());
-		
+
 		g.sortGarage();
-		
+
 		System.out.println("*** SORTED GARAGE ***");
 		System.out.println("DISPLAY MILEAGES:");
 		System.out.println(g.displayMileAges());
